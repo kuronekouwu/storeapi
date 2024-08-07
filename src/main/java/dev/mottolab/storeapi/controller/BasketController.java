eafbd4fdca3be5b109bd9dcfcd0416954723ddac
M307 <me@m307.dev>
2024-08-07 14:32:34 +0700

    feat: Add basket and order

---
+++ src/main/java/dev/mottolab/storeapi/controller/BasketController.java
@@ -0,0 +1,10 @@
+package dev.mottolab.storeapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/baskets")
+public class BasketController {
+
+}
--- src/main/java/dev/mottolab/storeapi/controller/OrderController.java
+++ src/main/java/dev/mottolab/storeapi/controller/OrderController.java
@@ -1,10 +1,14 @@
 package dev.mottolab.storeapi.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    @PostMapping("/user/createOrder")
+    public void creteOrder(){
 
+    }
 }
