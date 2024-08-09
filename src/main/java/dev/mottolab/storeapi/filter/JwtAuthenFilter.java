16a5a7802dd7551a76f7f048a5951715e21ece16
M307 <me@m307.dev>
2024-08-09 20:52:09 +0700

    fix: return 403 JSON

--- src/main/java/dev/mottolab/storeapi/filter/JwtAuthenFilter.java
+++ src/main/java/dev/mottolab/storeapi/filter/JwtAuthenFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
@@ -26,7 +28,7 @@ public class JwtAuthenFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, ResponseStatusException {
         // Get header
         String authorization = request.getHeader("Authorization");
         if(authorization != null && authorization.startsWith("Bearer ")) {
@@ -42,7 +44,12 @@ public class JwtAuthenFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }catch (Exception e){
-                e.printStackTrace();
+                logger.error(e.getMessage());
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write("{\"message\":\"Invalid token.\"}");
+                return;
             }
         }
         filterChain.doFilter(request, response);
