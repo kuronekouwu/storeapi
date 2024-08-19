045f9dbdfde237587ecb876cce709754b8045620
M307 <me@m307.dev>
2024-08-19 13:22:17 +0700

    feat: Add new security

--- src/main/java/dev/mottolab/storeapi/config/SecurityConfiguration.java
+++ src/main/java/dev/mottolab/storeapi/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
         return http.authorizeHttpRequests(
-                auth -> auth.requestMatchers("/authen/**", "/products/**", "/category/**").permitAll()
+                auth -> auth.requestMatchers("/authen/**", "/products/**", "/category/**", "/").permitAll()
                         .requestMatchers("/payment/callback/scb/completed", "/payment/callback/chillpay/completed", "/shipping/callback/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/products/create", "/category/create").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/products/**", "/category/**").authenticated()
